6851f6720a409bd503853cc1435214b3dfa3fb21
Esteban Perafan <bestebanperafan@gmail.com>
2024-08-29 20:24:23 -0400

    refactor: delete unused imports

--- src/main/java/com/eiji/dev/inventorymanagementsystem/controllers/Inventory.java
+++ src/main/java/com/eiji/dev/inventorymanagementsystem/controllers/Inventory.java
@@ -8,10 +8,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * @author Esteban Padilla
